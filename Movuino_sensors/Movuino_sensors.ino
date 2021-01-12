af8493c9af092dacdc91e8e9b41d2c8cfdf75633
PaullouisRossignol <handterpret@gmail.com>
2021-01-12 11:47:01 +0100

    add some display functions and correct function names for more comprehension

--- Movuino_sensors/Movuino_sensors.ino
+++ Movuino_sensors/Movuino_sensors.ino
@@ -1,27 +1,31 @@
-// MPU-6050 Short Example Sketch
-// By Arduino User JohnChi
-// August 17, 2014
-// Public Domain
 #include<Wire.h>
+
+// Déclation des variables
 const int MPU_addr=0x69; // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 byte MAX_CALIB_LOOP = 100;
 byte calibration_loop = MAX_CALIB_LOOP;
 bool calibration_bit = true;
-int time_elapsed_in_seconds = 0;
+const int16_t delay_per_loop = 100;
+const int16_t num_of_calibration_turn = 1000/delay_per_loop;
+int16_t calibration_tab[6][num_of_calibration_turn]
+int16_t calibration_index = 0;
+int16_t time_elapsed_in_seconds = 0;
 unsigned long StartTime;
+
 typedef struct{
-     int16_t max_val;
-     int16_t min_val;
+     int16_t max_drift_val;
+     int16_t min_drift_val;
      int16_t val;
 }metric;
 
 // table containing variables in this order : AcX, AcY, AcZ, GyX, GyY, GyZ
 metric metrics[6] = {{0, 0, 0},{0, 0, 0},{0, 0, 0},{0, 0, 0},{0, 0, 0},{0, 0, 0}};
-
+char *sensor_names[] = {"AcX", "AcY", "AcZ", "GyX", "GyY", "GyZ"};
 void init_calibration_values(metric (&values)[6]);
-void calibrate_values(metric (&metrics)[6]);
+void calibrate_sensor_drift(metric (&metrics)[6]);
 void display_metrics(metric (&metrics)[6]);
+void display_drift(metric (&metrics)[6]);
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -47,18 +51,17 @@ void loop(){
   if (calibration_bit) {
     if (calibration_loop <= MAX_CALIB_LOOP) {
       if (calibration_loop == MAX_CALIB_LOOP) {
-        Serial.println("Calibrating values...");
+        Serial.println("Calibrating standard drift, please don't move...");
         init_calibration_values(metrics);
-        display_metrics(metrics);
       }
       else {
-        calibrate_values(metrics);
+        calibrate_sensor_drift(metrics);
       }
       calibration_loop = calibration_loop - 1;
       //Serial.print("calibration_loop = ");Serial.println(calibration_loop);
     }
     else {
-      display_metrics(metrics);
+      display_drift(metrics);
       calibration_bit = false;
       StartTime = millis();
     }
@@ -68,32 +71,39 @@ void loop(){
     unsigned long ElapsedTime = CurrentTime - StartTime;
     if (ElapsedTime > (time_elapsed_in_seconds+1) * 1000) {
       time_elapsed_in_seconds ++;
-      Serial.print("And one more second 'til the end, ");Serial.print(time_elapsed_in_seconds); Serial.println(" seconds");
+      //Serial.print("And one more second 'til the end, ");Serial.print(time_elapsed_in_seconds); Serial.println(" seconds");
+      display_metrics(metrics);
     }
   }
   
-  delay(100);
+  delay(delay_per_loop);
 }
 
 void init_calibration_values(metric (&metrics)[6]){
   for(int i = 0; i < 6 ; i ++) {
-    metrics[i].max_val = metrics[i].val;
-    metrics[i].min_val = metrics[i].val;
+    metrics[i].max_drift_val = metrics[i].val;
+    metrics[i].min_drift_val = metrics[i].val;
   }
 }
 
-void calibrate_values(metric (&metrics)[6]) {
+void calibrate_sensor_drift(metric (&metrics)[6]) {
   for(int i = 0; i < 6 ; i ++) {
-    if ( metrics[i].max_val < metrics[i].val )
-      metrics[i].max_val = metrics[i].val;
-    else if ( metrics[i].min_val < metrics[i].val )
-      metrics[i].min_val = metrics[i].val;
+    if ( metrics[i].max_drift_val < metrics[i].val )
+      metrics[i].max_drift_val = metrics[i].val;
+    if ( metrics[i].min_drift_val > metrics[i].val )
+      metrics[i].min_drift_val = metrics[i].val;
   }
 }
-void display_metrics(metric (&metrics)[6]) {
+void display_drift(metric (&metrics)[6]) {
   Serial.println("Calibration Finished, order : AcX, AcY, AcZ, GyX, GyY, GyZ");
   Serial.println();
   for(int i = 0; i < 6 ; i ++) {
-    Serial.print("min = ");Serial.print(metrics[i].min_val);Serial.print(", max = ");Serial.println(metrics[i].max_val);
+    Serial.print("min = ");Serial.print(metrics[i].min_drift_val);Serial.print(", max = ");Serial.println(metrics[i].max_drift_val);
   }
+}
+void display_metrics(metric (&metrics)[6]) {
+  for(int i = 0; i < 6 ; i ++) {
+    Serial.print("sensor ");Serial.print(sensor_names[i]);Serial.print(" : ");Serial.println(metrics[i].val);
+  }
+  Serial.println();
 }
